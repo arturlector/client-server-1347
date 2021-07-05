3d3eb9c8d60038530b723bc16a22a5d4b506ad7e
stoicist <art.igberdin@gmail.com>
2021-07-05 20:35:25 +0300

    Initalize project

---
+++ client-server-1347/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client-server-1347
+//
+//  Created by Artur Igberdin on 05.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
